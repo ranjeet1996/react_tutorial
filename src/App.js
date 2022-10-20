f1e53e2c799f2ac8a5a9e3d7c06b74997f848255
Ranjeet Soni <ranjeetsoni1996@gmail.com>
2022-10-20 11:56:50 +0530

    React tutorial

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import Alert from "./components/Alert";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const [mode, setmode] = useState("light"); //whether dark mode is enable or not
